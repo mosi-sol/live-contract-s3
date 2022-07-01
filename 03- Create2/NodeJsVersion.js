6d4c9659641c2e8eb64540c09198ede0ca729025
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-07-01 17:35:58 +0430

    Create NodeJsVersion.js

---
+++ 03- Create2/NodeJsVersion.js
@@ -0,0 +1,22 @@
+// credit for a guy build this around the web
+// nodejs solution create2
+// node version: v9.10.0
+// module versions:
+// rlp@2.0.0
+// keccak@1.4.0
+
+const rlp = require("rlp");
+const keccak = require("keccak");
+
+var nonce = 0x00; //The nonce must be a hex literal!
+var sender = "0x6ac7ea33f8831ea9dcc53393aaa88b25a785dbf0"; //Requires a hex string as input!
+
+var input_arr = [sender, nonce];
+var rlp_encoded = rlp.encode(input_arr);
+
+var contract_address_long = keccak("keccak256")
+  .update(rlp_encoded)
+  .digest("hex");
+
+var contract_address = contract_address_long.substring(24); //Trim the first 24 characters.
+console.log("contract_address: " + contract_address);
