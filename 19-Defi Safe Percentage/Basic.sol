cf634a2e1cb59c0558d55bb6625eb7b3949e35f3
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-09-15 17:03:13 +0430

    Create Basic.sol

---
+++ 19-Defi Safe Percentage/Basic.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract SafePercentage {
+    using SafeMath for uint256;
+
+    uint256 percentage;
+
+    function setPercentageFee(uint256 fee) external {
+         percentage = fee;
+    }
+    
+    function transferFrom(uint256 amount) public view returns (uint256 percentageFee, uint256 total) { 
+        percentageFee = (amount.mul(percentage)).div(10000);
+        total = amount.sub(percentageFee);
+    }
+
+}
